29235226ffdf423b4d51d14a3ba36cb99563e435
Todd Morningstar <techarchist@gmail.com>
2015-11-11 20:13:30 -0500

    Refs #5 - Fix typo for ROLE_TYPES const check;

--- app/models/concerns/roles.rb
+++ app/models/concerns/roles.rb
@@ -2,7 +2,7 @@
 #  procedures, and structures.
 module Roles
   # @see comment in Proposal::State for issue on consts defined 2x.
-  unless const_defined?(:ROLES)
+  unless const_defined?(:ROLE_TYPES)
     ROLE_ORGANIZER = 'organizer'
     ROLE_REVIEWER  = 'reviewer'
     ROLE_TYPES     = [ROLE_ORGANIZER, ROLE_REVIEWER]
